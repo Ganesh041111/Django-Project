fb2c9851e357dee844e1eac645b3a8b6a86d4c78
Ganesh041111 <Ganeshshingre07@gmail.com>
2023-07-07 17:08:39 +0530

    Email Validation

--- Ecommerce-Website/ecommerce/authcart/views.py
+++ Ecommerce-Website/ecommerce/authcart/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User 
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from .utils import TokenGenerator, generate_token
+from django.utils.encoding import force_bytes, force_str, DjangoUnicodeDecodeError
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.views.generic import View
 
 # Create your views here.
 def signup(request):
@@ -8,17 +16,46 @@ def signup(request):
         password=request.POST["pass1"]
         confirm_password=request.POST["pass2"]
         if password!=confirm_password:
-            return HttpResponse("Incorrect Password")
+            messages.error(request,"Passwords are not matching!!")
+            return render(request, "signup.html")
         try:
             if User.objects.get(username=email):
-                return HttpResponse("User already exist")
-            
+                messages.info(request,"Email is already taken!!")
+                return render(request, "signup.html")
         except Exception as identifier:
             pass
 
         user=User.objects.create_user(email,email,password)
+        user.is_active=False
         user.save()
+        email_subject="Activate Your Account"
+        message=render_to_string('activate.html',{
+            'user':user,
+            'domain':'127.0.0.1:8000',
+            'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+            'token':generate_token.make_token(user)
+        })
+
+        email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
+        email_message.send()
+        messages.success(request, "Activate your account by clicking the link in your Email.")
+        return redirect('/auth/login/')
+
     return render(request, "signup.html")
+
+class ActivateAccountView(View):       #class based function
+    def get(self, request, uidb64, token):
+        try:
+            uid=force_str(urlsafe_base64_decode(uidb64))
+            user=User.objects.get(pk=uid)
+        except Exception as identifier:
+            user=None 
+        if user is not None and generate_token.check_token(user, token):
+            user.is_active=True
+            user.save()
+            messages.info(request, "Account activated successfully!!")
+            return redirect ('/auth/login')
+        return render(request,'activatefail.html')
 
 def handlelogin(request):
     return render(request, "login.html")
--- Ecommerce-Website/ecommerce/ecommerce/settings.py
+++ Ecommerce-Website/ecommerce/ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,6 +114,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+# sending emails
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_HOST_USER= ''
+EMAIL_HOST_PASSWORD= ''
+EMAIL_PORT= 587
+EMAIL_USE_TLS= True
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -129,3 +137,7 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
